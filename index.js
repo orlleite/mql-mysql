84bb947765461938eebe3d0d6aa2dbebaa5d35dc
Orlando Leite <orlleite@gmail.com>
2018-08-19 18:52:08 -0400

    path adjusted

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 module.exports = 
 {
-	MySQL:require('lib/mysql'),
-	MQL:require('lib/mql'),
-	MQLtoMySQL:require('lib/mql-to-mysql')
+	MySQL:require('./lib/mysql'),
+	MQL:require('./lib/mql'),
+	MQLtoMySQL:require('./lib/mql-to-mysql')
 }
